66e2467bbe261f97a84c5688222cf2cd9361c451
Subhikshaa23 <210701264@rajalakshmi.edu.in>
2024-02-19 13:43:31 +0530

    Profile page done

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 const ProfilePage = () => {
   return (
     <div>
-      
+      <div>
+        <img src="/user.png" alt="user icon" style={{ display: 'inline-block' }} height={50} width={50} />
+        <h1>Subhikshaa S</h1>
+      </div>
+    
+      <h4>Email: </h4>
+      <p>21060@rajalakshmi.edu.in</p>
+
+      <h4>Mobile Number: </h4>
+      <p>2106098765</p>
+
+      <h4>Location:  </h4>
+      <p>654 565</p>
+
     </div>
   )
 }
