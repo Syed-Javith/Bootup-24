0e7dd4cbbde3c6a6365857706ae4828f4382feaa
Syed-Javith <syedjavith14@gmail.com>
2024-02-19 13:57:56 +0530

    uodated

--- client/src/pages/FoodItem.jsx
+++ client/src/pages/FoodItem.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSearchParams , useParams } from 'react-router-dom'
+import axios from'axios'
 
 const FoodItem = () => {
+  const params = useParams();
+  console.log(params.id);
+
+  const [food , setFood] = useState()
+  const getFood = async () => {
+    try {
+      const res = await axios.get('http://localhost:8000/food/'+params.id)
+      setFood(res.data)
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(()=> {
+    getFood()
+  })
   return (
     <div>
-      <h1>{}</h1>
+      <h1>{food?.description}</h1>
     </div>
   )
 }
--- client/src/pages/FoodPage.jsx
+++ client/src/pages/FoodPage.jsx
@@ -1,31 +1,53 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios"
+import Cookies from 'universal-cookie'
+import { Link } from 'react-router-dom';
 const FoodPage = () => {
     const [foods, setFoods] = useState([]);
+    const [user , setUser] = useState(null)
+    const cookies = new Cookies();
     const getFoods = async () => {
         try {
             const res = await axios.get('http://localhost:8000/food')
             setFoods(res.data)
         } catch (error) {
-            console.log(error);
+            // console.log(error);
         }
     }
     useEffect(() => {
+        const userCookie = cookies.get('user')
+        if(userCookie) setUser(userCookie)
+        console.log(user);
         getFoods();
-    })
+    },[])
     return (
         <div>
             {
-                foods.map((food) => {
-                    return <div className='container'>
+                user?.username
+            }
+           {
+
+            user?.role === 'D' && <>
+             <button>Post food availability</button>
+            <button>View request</button>
+            </>
+           }
+            
+                <div className='food-cont'>
+                {foods.map((food) => {
+                    return <Link to={`/foods/${food._id}`}>
+                    <div className='container'>
                         <img src={food.image} height={300} width={300} />
                         <p> <span className='title'> Foods Available </span> {food.foods}</p>
                         <p>{food.description}</p>
                         <p>{food.location}</p>
                         <p>{food.distributor}</p>
                     </div>
+                    </Link>
                 })
             }
+                </div>
+            
         </div>
     )
 }
