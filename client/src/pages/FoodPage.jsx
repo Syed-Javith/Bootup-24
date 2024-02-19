d20b365c9aba46a71a79c4d96a77ad650c701871
Syed-Javith <syedjavith14@gmail.com>
2024-02-19 12:55:04 +0530

    Server finished for posts

--- client/src/pages/FoodPage.jsx
+++ client/src/pages/FoodPage.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
 const FoodPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [foods, setFoods] = useState([]);
+    const getFoods = async () => {
+        try {
+            const res = await axios.get('http://localhost:8000/food')
+            setFoods(res.data)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        getFoods();
+    })
+    return (
+        <div>
+            {
+                foods.map((food) => {
+                    return <div className='container'>
+                        <img src={food.image} height={300} width={300} />
+                        <p> <span className='title'> Foods Available </span> {food.foods}</p>
+                        <p>{food.description}</p>
+                        <p>{food.location}</p>
+                        <p>{food.distributor}</p>
+                    </div>
+                })
+            }
+        </div>
+    )
 }
 
 export default FoodPage
